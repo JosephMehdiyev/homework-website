ab0b9887605eaa9e8f485effe8e82d47e5b91a44
ilham Hajiyev <ilhamhaciyev1@gmail.com>
2024-11-26 15:39:20 +0400

    Added operational color theme button(color choice to be discussed later); Added a feature whereby dark mode is switched on automatically after 18:00 pm(in web-project.html only)

--- scripts.js
+++ scripts.js
@@ -47,3 +47,37 @@ function DrawSpiral(context) {
         turnRight(89);
     }
 }
+var darkmode="inactive"
+var time = new Date().getHours();
+var button = document.getElementById("darkmodebutton")
+
+
+
+const switchdarkmode =  ()=>{            
+    document.getElementById('dmp').style.display = "block"
+    document.getElementById('lmp').style.display = "none"
+    document.body.style.backgroundColor = "rgb(128,128,128)"
+    darkmode= "active"
+}
+const switchlightmode =  ()=>{
+    document.getElementById('dmp').style.display = "none"
+    document.getElementById('lmp').style.display = "block"
+    document.body.style.backgroundColor = "rgb(255,255,255)"
+    darkmode= "inactive"
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("darkmodebutton");
+
+        button.addEventListener("click", () => {
+            if (darkmode === "active") {
+                switchlightmode();
+            } else {
+                switchdarkmode();
+            }
+        });        
+    });
+
+if (time===18){
+    switchdarkmode()
+}
