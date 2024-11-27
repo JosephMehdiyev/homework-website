32c5444924489c7a7c16908f7fb87d0a767916b1
Yusif Mehdiyev <yusifmehdiyev55@gmail.com>
2024-11-28 02:43:20 +0400

    added nav button color identicator

--- scripts.js
+++ scripts.js
@@ -2,7 +2,21 @@
     Please write all of your website related scripts in this file. DO NOT write anything script related in code-artist.js
 */
 
+
+/* Scripts related to the navigaton var*/
+const navButtons = document.getElementsByClassName('nav-buttons');
+const currentPath = window.location.href;
+for (let i = 0; i < navButtons.length; i++) {
+    const link = navButtons[i].href;
+
+    if (link === currentPath) {
+        navButtons[i].style.color = 'red';
+    }
+}
+/* Navigation script ends here*/
+
 /*code for colot theme button */
+
 var darkmode = "inactive"
 var time = new Date().getHours();
 var button = document.getElementById("darkmodebutton")
