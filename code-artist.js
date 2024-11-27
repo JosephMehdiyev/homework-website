57ade7c7b38f6bf6e8f0f4097fa331f43f4a115e
Yusif Mehdiyev <yusifmehdiyev55@gmail.com>
2024-11-27 21:30:50 +0400

    addde comments, soon will make the art more complex

--- code-artist.js
+++ code-artist.js
@@ -18,12 +18,19 @@ class Point {
     }
 }
 
+// A function that draws a circle
 function drawCircle(center, radius) {
+    // Start drawing our path
     cartesianPlane.beginPath();
+    // To draw circle, we add bunch of rotated radii to the center position
+    // We use trigonometric functions for this purpose
     for (let i = 0; i <= 360; i++) {
         let angle = i * Math.PI / 180;
         let x = center.x + radius * Math.cos(angle);
         let y = center.y + radius * Math.sin(angle);
+        // Dunno why but there was a duplicate circle without this if statements
+        // Changing above loop inequality to " < " from " <= " did not help too.
+        // This if statements fixes it
         if (i === 0) {
             cartesianPlane.moveTo(x, y);
         } else {
@@ -31,14 +38,16 @@ function drawCircle(center, radius) {
         }
     }
     cartesianPlane.closePath();
+    // We draw our path here
     cartesianPlane.stroke();
 }
 
 
 var center = new Point(cartesianPlane.canvas.width / 2, cartesianPlane.canvas.height / 2);
-var radius = 100;
-cartesianPlane.strokeStyle = `rgb(${255 * Math.random()}, ${255 * Math.random()}, ${255 * Math.random()})`;
-for (let j = 0; j <= 50; j++) {
+var radius = 150;
+cartesianPlane.strokeStyle = `rgb(${0}, ${0}, ${255})`;
+
+for (let j = 0; j <= 24; j++) {
     for (let i = 0; i <= 360; i += 60) {
 
         let angle = i * Math.PI / 180 + j;
@@ -46,6 +55,9 @@ for (let j = 0; j <= 50; j++) {
         center.x = center.x + radius * Math.cos(angle);
         center.y = center.y + radius * Math.sin(angle);
     }
+    // The above loop changes the center of the cartesian plane
+    // It could be done better, but as a secondary solution,
+    // We have to set it back to the original center of the canvas/plane
     center.x = cartesianPlane.canvas.width / 2;
     center.y = cartesianPlane.canvas.height / 2;
 }
